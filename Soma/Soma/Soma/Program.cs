b1826878423c22489d0bca990cdec7d681ba17b9
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-18 12:42:16 -0300

    Add files via upload

---
+++ Soma/Soma/Soma/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Soma");
+
+Console.WriteLine("Olá! Digite um número: ");
+
+float n1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro número: ");
+
+float n2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro número: ");
+
+float n3 = float.Parse(Console.ReadLine());
+
+if (n1 < n2 && n1 < n3 )
+{
+    double s = n2 + n3;
+
+    Console.WriteLine($"Os maiores números informados foram {n2} e {n3} e a soma entre eles é igual a: {s}");
+}
+if (n2 < n1 && n2 < n3)
+{
+    double s = n1 + n3;
+
+    Console.WriteLine($"Os maiores números informados foram {n1} e {n3} e a soma entre eles é igual a: {s}");
+}
+if (n3 < n2 && n3 < n1)
+{
+    double s = n1 + n2;
+
+    Console.WriteLine($"Os maiores números informados foram {n1} e {n2} e a soma entre eles é igual a: {s}");
+}
---
+++ Zero/Zero/Zero/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Zero");
+
+Console.WriteLine("Olá! Escreva um número: ");
+
+float n1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Escreva outro número: ");
+
+float n2 = float.Parse(Console.ReadLine());
+
+while (n2 <= 0)
+{
+    Console.WriteLine("Por favor escreva um número maior que 0:");
+    n2 = float.Parse(Console.ReadLine());
+}
+
+double r = n1 + n2;
+
+Console.WriteLine($"A soma entre os números {n1} e {n2} é igual a: {r.ToString("N2")}");
