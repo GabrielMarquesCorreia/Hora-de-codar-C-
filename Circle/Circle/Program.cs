30ed907dd32bb2e4a5174dc1f452ef4072dc8c8a
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-18 12:37:24 -0300

    Add files via upload

---
+++ Circle/Circle/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Área do círculo");
+
+Console.WriteLine("Escreva a base de um círculo: ");
+
+float b = float.Parse(Console.ReadLine());
+
+double r = 3.14 * b;
+
+Console.WriteLine($"A área deste círculo é igual a: {r.ToString("N2")}");
---
+++ Losango/Losango/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Área do losango");
+
+Console.WriteLine("Digite a diagonal maior de um losango: ");
+
+float dM = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Agora escreva a diagonal menor deste loosango: ");
+
+float dm = float.Parse(Console.ReadLine());
+
+double r = (dM * dm) / 2;
+
+Console.WriteLine($"A área deste losango é igual a: {r.ToString("N2")}");
